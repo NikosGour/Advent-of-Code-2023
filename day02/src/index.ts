0d3c3fb8fce76dcf82ed97ef3c20f03118c9eb83
NikosGour <nikosgournak@gmail.com>
2024-04-23 02:11:13 +0300

    feat: started day 2

---
+++ day02/src/index.ts
@@ -0,0 +1,22 @@
+import * as fs from "fs";
+const data:string[] = fs.readFileSync(`input.txt`, `utf-8`).split(`\n`);
+for (const i in data) {
+	const line = data[ i ];
+	const peos = 2+ 3;
+}
+const part1 = ():number => { return 0; };
+const part2 = ():number => { return 0; };
+class nikos{
+	constructor(){
+
+	}
+
+	public peos(){}
+
+	public peos2(){}
+
+	public peos3(){}
+}
+
+//part1();
+//part2();
